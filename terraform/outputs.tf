d8bece702d30c520323d4ca7f7a61a4eb419f80e
Daniel Willson <danielwillson004@gmail.com>
2025-10-28 12:14:43 +0530

    n

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,5 +2,5 @@
 
 output "api_service_url" {
   description = "https://diabetes-api-service-645615724525.us-central1.run.app"
-  value = google_cloud_run_service.default.status[0].url
+  value       = google_cloud_run_v2_service.api_service.uri
 }
