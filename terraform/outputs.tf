09a93439fda1020936f7aa34745e96bb55554870
Daniel Willson <danielwillson004@gmail.com>
2025-10-28 12:24:47 +0530

    n

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,9 @@
-# terraform/outputs.tf
-
-output "api_service_url" {
-  description = "https://diabetes-api-service-645615724525.us-central1.run.app"
+# This name MUST match what's in your GitHub workflow
+output "cloud_run_url" {
+  
+  # The description is just a human-readable string, not the value
+  description = "The URL of the deployed Cloud Run service"
+  
+  # This correctly gets the URL from the resource named "api_service" in your main.tf
   value       = google_cloud_run_v2_service.api_service.uri
 }
