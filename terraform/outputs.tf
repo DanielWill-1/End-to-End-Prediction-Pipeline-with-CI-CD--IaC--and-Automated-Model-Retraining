9c048e7f23e21ac11e64b81482c9b36ca29aeae0
Daniel Willson <danielwillson004@gmail.com>
2025-10-28 12:07:32 +0530

    v

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 # terraform/outputs.tf
 
 output "api_service_url" {
-  description = "The public URL of the deployed FastAPI service."
-  value       = google_cloud_run_v2_service.api_service.uri
+  description = "https://diabetes-api-service-645615724525.us-central1.run.app"
+  value = google_cloud_run_service.default.status[0].url
 }
